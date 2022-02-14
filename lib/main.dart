44736e19426475dfb2d281c355cc5e7144a52a7f
hasanaliozdemir <68190862+hasanaliozdemir@users.noreply.github.com>
2022-02-14 23:20:33 +0300

    debug banner :)

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Codelabs 1',
       theme: ThemeData(
         primarySwatch: Colors.blue,
