5548347e3b9e6a298dca692e3c89403ef0bd4717
hasanaliozdemir <68190862+hasanaliozdemir@users.noreply.github.com>
2022-02-14 23:19:20 +0300

    home_page

---
+++ lib/home_page/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:codelabs_1/home_page/home_funcs.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: HomeFuncs().buildAppBar(context),
+      body: HomeFuncs().buildBody(),
+    );
+  }
+
+  
+}
